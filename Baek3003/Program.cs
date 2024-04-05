398f67875b9ac6d8c83622a6fc2aa7b0780ddd92
rapidswap <curudow7988@gmail.com>
2024-04-05 14:48:41 +0900

    new file:   Baek3003.sln 	new file:   Baek3003/Baek3003.csproj 	new file:   Baek3003/Program.cs 	new file:   Baek3003/obj/Baek3003.csproj.nuget.dgspec.json 	new file:   Baek3003/obj/Baek3003.csproj.nuget.g.props 	new file:   Baek3003/obj/Baek3003.csproj.nuget.g.targets 	new file:   Baek3003/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Baek3003/obj/Debug/net8.0/Baek3003.AssemblyInfo.cs 	new file:   Baek3003/obj/Debug/net8.0/Baek3003.AssemblyInfoInputs.cache 	new file:   Baek3003/obj/Debug/net8.0/Baek3003.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Baek3003/obj/Debug/net8.0/Baek3003.GlobalUsings.g.cs 	new file:   Baek3003/obj/Debug/net8.0/Baek3003.assets.cache 	new file:   Baek3003/obj/project.assets.json 	new file:   Baek3003/obj/project.nuget.cache

---
+++ Baek3003/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baek3003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] white_chess=new int[6]{1,1,2,2,2,8};
+            int needchess=0;
+            string input; 
+            input = Console.ReadLine();
+            string[] find_chess = input.Split(' ');
+            for (int i=0; i<6; i++){
+                needchess=(int)white_chess[i] - int.Parse(find_chess[i]);
+                Console.Write(needchess+" ");
+                needchess=0;
+            }
+
+        }
+    }
+}
